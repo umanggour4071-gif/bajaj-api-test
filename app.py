041074c9a76d34c4b75a5cd8d3514ec93f6f78e9
umanggour4071-gif <umanggour4071@gmail.com>
2026-05-26 15:40:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return {"message": "API Working Successfully"}
+    return {
+        "message": "API Running"
+    }
+
+@app.route('/health')
+def health():
+    return {
+        "status": "healthy"
+    }
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
